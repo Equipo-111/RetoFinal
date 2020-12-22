e1ce58d63709b7d6526902c087a202fd5fec8e4e
JorAndMo <ing.andresm@gmail.com>
2020-12-22 16:57:25 -0500

    instalación axios

---
+++ src/views/Nosotros.vue
@@ -0,0 +1,12 @@
+<template>
+    <nosotros/>
+</template>
+
+<script>
+import TheNosotros from '../components/TheNosotros.vue'
+export default {
+name: "TheNosotros",
+  components: { TheNosotros },
+    
+}
+</script>
